2f69b114e51a91717cc1545c9ddca436170ea102
emilyli1222 <46393272+emilyli1222@users.noreply.github.com>
2020-02-25 19:02:55 -0800

    yuanrui_code

---
+++ runner.py
@@ -0,0 +1,53 @@
+import datetime as dt
+import numpy as np
+from simulator import Simulator
+from actor_critic_agents import ActorAgent
+
+"""
+        
+        Code based on previous code written by Yichen Shen and Yiding Zhao 
+        ( <https://github.com/shenyichen105/Deep-Reinforcement-Learning-in-Stock-Trading> )
+        
+"""
+
+def main():
+    actions = ["buy", "sell", "hold"]
+
+    environment = Simulator(['ITSA4', 'ITUB3'], dt.datetime(2007, 1, 1), dt.datetime(2018, 1, 1))
+
+    # Creates instance of agent
+    agent = ActorAgent(lookback=environment.init_state())
+    
+    # Choice of first action
+    action = agent.init_query()
+    print ('choice of first action',action)
+
+    step = 0
+
+    while environment.has_more():
+
+            # Maps action from id to name
+
+            action = actions[action]
+            print ('action from action list',action)
+
+            # Simulation step (trading day). Apart from reward and state, the simulator provides the agent with the current day 
+            # and information concerning restrictions (boundary). The agent uses the first one in the computation of a kind of
+            # epsilon-greedy policy; the second piece of information serves to limit the ability of the agent to open or close
+            # positions in certain scenarios
+            step +=1
+            print ('for step', step)
+            #reward, state, day, boundary = environment.step(action)
+            reward, state, day = environment.step(action)
+
+            # Agent takes action
+            #action = agent.query(state, reward, day, boundary)
+            action = agent.query(state,reward,day)
+
+
+if __name__ == '__main__':
+    main()
+
+
+#reward, state, pair result, how RNN works
+#five action currently
